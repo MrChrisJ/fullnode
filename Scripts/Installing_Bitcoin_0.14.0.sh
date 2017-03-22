ab31eb55c0896d5b3548b596d8c3fc05759d2d40
Olliecad1 <officialgamermorris@gmail.com>
2017-03-22 16:15:47 +0000

    update

--- Scripts/Installing_Bitcoin_0.14.0.sh
+++ Scripts/Installing_Bitcoin_0.14.0.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 # Install dependencies for Bitcoin Core (not the GUI)
-sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-de$
+sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libevent-dev -y
 # Install dependencies for Bitcoin QT (GUI)
 sudo apt-get install libqt4-dev qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y
 # Setup Swap file
@@ -23,7 +23,7 @@ git clone -b v0.14.0 https://github.com/bitcoin/bitcoin
 cd bitcoin/
 ./autogen.sh
 ./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default --with-gui=qt4
-make -j2
+make
 sudo make install
 sudo rm -r -f /home/pi/bin
 mkdir ~/.bitcoin/
