69814f3cef9da9cdc456b0d59ffa8571f9ef86a7
Olliecad1 <officialgamermorris@gmail.com>
2017-03-22 16:07:37 +0000

    update

--- Scripts/Installing_Bitcoin_0.14.0.sh
+++ Scripts/Installing_Bitcoin_0.14.0.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 # Install dependencies for Bitcoin Core (not the GUI)
-sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libevent-dev -y
+sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-de$
 # Install dependencies for Bitcoin QT (GUI)
 sudo apt-get install libqt4-dev qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y
 # Setup Swap file
@@ -23,15 +23,14 @@ git clone -b v0.14.0 https://github.com/bitcoin/bitcoin
 cd bitcoin/
 ./autogen.sh
 ./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default --with-gui=qt4
-make
+make -j2
 sudo make install
 sudo rm -r -f /home/pi/bin
-mkdir /home/pi/.bitcoin/
-cd .bitcoin/
-touch bitcoin.conf
+mkdir ~/.bitcoin/
+touch ~/.bitcoin/bitcoin.conf
 printf 'listen=1\nserver=1\ndaemon=0\ntestnet=0\nmempoolexpiry=72\nmaxmempool=300\nmaxorphantx=100\nlimitfreerelay=10\nminrelaytxfee=0.0001\nmaxconnections=40\n' >> bitcoin.conf
 
 # Remove the swap file we made earlier
 sudo swapoff -a
 sudo rm /var/swap
-sudo systemctl disable dphys-swapfilewap file
+sudo systemctl disable dphys-swapfile
