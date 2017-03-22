356110abd0b19008ede99915908a24f54688ee99
Olliecad1 <officialgamermorris@gmail.com>
2017-03-22 15:44:00 +0000

    Update

--- Scripts/Installing_Bitcoin_0.14.0.sh
+++ Scripts/Installing_Bitcoin_0.14.0.sh
@@ -1,20 +1,16 @@
-#!/bin/sh
 
+#!/bin/sh
 # Install updates
 sudo apt-get update
 sudo apt-get upgrade -y
-
 # Install dependencies for Bitcoin Core (not the GUI)
-sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libevent-dev -y
-
+sudo apt-get install build-essential autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libb$
 # Install dependencies for Bitcoin QT (GUI)
 sudo apt-get install libqt4-dev qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y
-
 # Setup Swap file
-sudo sed -i '16s/.*/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo sed -i '16s/.*/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
-
 mkdir ~/bin
 cd ~/bin
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
@@ -28,16 +24,15 @@ git clone -b v0.14.0 https://github.com/bitcoin/bitcoin
 cd bitcoin/
 ./autogen.sh
 ./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default --with-gui=qt4
-make -j2
+make
 sudo make install
 sudo rm -r -f /home/pi/bin
 mkdir /home/pi/.bitcoin/
 cd .bitcoin/
 touch bitcoin.conf
-printf 'listen=1\n   server=1\n  daemon=1\n   testnet=0\n   rpcuser=BITCOIN_USER\n   rpcpassword=BITCOIN_PASSWORD' >> bitcoin.conf
-
+printf 'listen=1\nserver=1\ndaemon=0\ntestnet=0\nmempoolexpiry=72\nmaxmempool=300\nmaxorphantx=100\nlimitfreerelay=10\nminrelaytxfee=0.0001\nmaxconnections=40\n' >> bi$
 
 # Remove the swap file we made earlier
-sudo chmod -x /etc/init.d/dphys-swapfile
 sudo swapoff -a
 sudo rm /var/swap
+sudo systemctl disable dphys-swapfilewap file
